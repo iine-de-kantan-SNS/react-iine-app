b90819afe3698b74c6589403c39a2696e77b2796
sasukemiyazawa <b1020251@fun.ac.jp>
2022-08-05 18:02:54 +0900

    styled更新

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,11 +1,16 @@
 import image from '../ojisan.png'
+import styled from 'styled-components';
 const Image = () => {
 
     return(
         <>
-        <img src={image}/>
+        <Sasuke src={image}/>
         </>
     )
 }
 
 export default Image;
+
+const Sasuke = styled.img`
+    background-color: red;
+`
