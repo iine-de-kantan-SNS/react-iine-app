4e1f5fb36f95e774ee45b18ced07b3360df2e477
sasukemiyazawa <b1020251@fun.ac.jp>
2022-08-05 15:27:53 +0900

    first commit

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+const Title = () => {
+    return(
+        <StyledH1>あああ</StyledH1>
+    )
+}
+
+export default Title;
+
+const StyledH1 = styled.h1`
+    color: red;
+`
